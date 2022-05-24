b01f0fbcab05b0d93e1da6331e7ff0cdcf2ac638
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-24 11:12:58 +0600

    mongodb,cors installed and carparts db connected

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('HELLO THERE!!');
 });
+
+const uri = `mongodb+srv://${process.env.DB_AUTHOR}:${process.env.DB_PASSWORD}@cluster0.cvx4k.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const carPartsCollection = client.db('carParts').collection('parts');
+
+    // displaying all the car parts
+    app.get('/car-parts', async (req, res) => {
+      const query = {};
+      const cursor = carPartsCollection.find(query);
+      const carParts = await cursor.toArray();
+      res.send(carParts);
+    });
+  } finally {
+    // await client.close();
+  }
+};
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);

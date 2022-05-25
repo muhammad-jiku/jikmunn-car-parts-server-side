71b2931cfd89b2ee499c6021afaad4325d483e26
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-25 15:52:24 +0600

    user inserting to database or updating user email in database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
@@ -14,6 +15,27 @@ app.get('/', (req, res) => {
   res.send('HELLO THERE!!');
 });
 
+// verifying JWT
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res
+      .status(401)
+      .send({ message: 'Access to this route is unauthorized' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err)
+      return res
+        .status(403)
+        .send({ message: 'Access to this route is forbidden' });
+  });
+  req.decoded = decoded;
+  console.log('decoded ', decoded);
+  console.log('Auth header ', authHeader);
+  next();
+};
+
 const uri = `mongodb+srv://${process.env.DB_AUTHOR}:${process.env.DB_PASSWORD}@cluster0.cvx4k.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -25,6 +47,7 @@ const run = async () => {
   try {
     await client.connect();
     const carPartsCollection = client.db('carParts').collection('carPart');
+    const usersCollection = client.db('users').collection('user');
 
     // displaying all the car parts
     app.get('/car-parts', async (req, res) => {
@@ -35,12 +58,37 @@ const run = async () => {
     });
 
     // displaying single car part for purchase
-    app.get('/car-parts/:carItemId', async (req, res) => {
+    app.get('/car-parts/:carItemId', verifyJWT, async (req, res) => {
       const id = req.params.carItemId;
       const query = { _id: ObjectId(id) };
       const carItem = await carPartsCollection.findOne(query);
-      // const  = cursor;
       res.send(carItem);
+    });
+
+    // issuing JWT during signin or signup or social login
+    // inserting user to database if user does not exist
+    // updating uset email if does exist
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateUser = {
+        $set: user,
+      };
+      const usersResult = await usersCollection.updateOne(
+        filter,
+        updateUser,
+        options
+      );
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: '1d',
+        }
+      );
+      res.send({ usersResult, accessToken: token });
     });
   } finally {
     // await client.close();
